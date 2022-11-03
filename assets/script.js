5c075549f7f474767c99f049ea05b9e2bd7eb8ea
Ryan Stephens <ryandstephens6@gmail.com>
2022-11-03 16:52:08 -0600

    Quiz is now fully functional

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//Variable declaration
 var quizElement = document.getElementById("quiz");
 var startButton = document.getElementById("start-button");
 var mainElement = document.querySelector("main");
@@ -7,22 +8,25 @@ var button2Element = document.getElementById("btn2");
 var button3Element = document.getElementById("btn3");
 var button4Element = document.getElementById("btn4");
 var score = 0;
-var time = 120;
+var timer = 120;
 var currentQuestion = 0;
 
+//Generates question objects that contain the question, 4 options, and the correct answer
 const question1 = {question: "Why is the sky blue", options: ["wrong", "right", "wrong", "wrong"], answer: "right"}
 const question2 = {question: "Why is the sky blue", options: ["wrong", "wrong", "wrong", "right"], answer: "right"}
 const question3 = {question: "Why is the sky blue", options: ["wrong", "wrong", "wrong", "right"], answer: "right"}
 
+//Creates list of question objects
 var questionList = [question1, question2, question3];
-var buttonList = [button1Element, button2Element, button3Element, button4Element]
 
+//Starts the quiz by hiding the homepage and rendering the first question
 function startQuiz() {
-    mainElement.classList.add("hidden");
+    mainElement.style="display:none";
     renderQuestion(questionList[0]);
-    quizElement.classList.remove("hidden");
+    quizElement.style="display:flex";
 }
 
+//Renders a question by adding text from a question object to the text of the question element and button elements
 function renderQuestion(question) {
     questionElement.textContent = question.question;
     button1Element.textContent = question.options[0];
@@ -31,6 +35,7 @@ function renderQuestion(question) {
     button4Element.textContent = question.options[3];
 }
 
+//Checks if the button the user selected is the button to the correct answer. If it is the correct button, the user will gain a point and the next question in the question list will be rendered. If incorrect, the user will lose time and the next question will be rendered
 function checkAnswer(button) {
     console.log(button.textContent);
     if(button.textContent === questionList[currentQuestion].answer) {
@@ -44,7 +49,7 @@ function checkAnswer(button) {
         }
         
     } else {
-        //subtract timer
+        timer -= 10;
         currentQuestion++;
         if(currentQuestion >= questionList.length) {
             renderEndPage();
@@ -56,8 +61,9 @@ function checkAnswer(button) {
 }
 
 function renderEndPage() {
-    mainElement.classList.remove("hidden");
-    quizElement.classList.add("hidden");
+    mainElement.style="display:none";
+    quizElement.style="display:none";
+    
 }
 
 startButton.addEventListener("click", startQuiz);

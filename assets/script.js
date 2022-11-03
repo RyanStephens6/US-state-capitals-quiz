49f891c280a5b8d0dcaa28c060dc8c2bcd0506aa
Ryan Stephens <ryandstephens6@gmail.com>
2022-11-03 15:08:29 -0600

    Added working start button

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
-var questionElement = document.getElementById("#questions");
+var questionElement = document.getElementById("questions");
 var startButton = document.getElementById("start-button");
 var mainElement = document.querySelector("main");
 
+
+
 function startQuiz() {
     mainElement.classList.add("hidden");
+    questionElement.classList.remove("hidden");
 }
 
-startButton.addEventListener("click", startQuiz())
+startButton.addEventListener("click", startQuiz)

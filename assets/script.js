039f0bc87021b8b9a37dee48f2814572b7c5bd04
Ryan Stephens <ryandstephens6@gmail.com>
2022-11-03 16:39:33 -0600

    Added functionality to quiz

--- assets/script.js
+++ assets/script.js
@@ -6,12 +6,16 @@ var button1Element = document.getElementById("btn1");
 var button2Element = document.getElementById("btn2");
 var button3Element = document.getElementById("btn3");
 var button4Element = document.getElementById("btn4");
+var score = 0;
+var time = 120;
+var currentQuestion = 0;
 
-const question1 = {question: "what is", option1: "wrong", option2: "wrong", option3: "wrong", answer: "right"}
-const question2 = {question: "what is", option1: "wrong", option2: "wrong", option3: "wrong", answer: "right"}
-const question3 = {question: "what is", option1: "wrong", option2: "wrong", option3: "wrong", answer: "right"}
+const question1 = {question: "Why is the sky blue", options: ["wrong", "right", "wrong", "wrong"], answer: "right"}
+const question2 = {question: "Why is the sky blue", options: ["wrong", "wrong", "wrong", "right"], answer: "right"}
+const question3 = {question: "Why is the sky blue", options: ["wrong", "wrong", "wrong", "right"], answer: "right"}
 
 var questionList = [question1, question2, question3];
+var buttonList = [button1Element, button2Element, button3Element, button4Element]
 
 function startQuiz() {
     mainElement.classList.add("hidden");
@@ -21,10 +25,43 @@ function startQuiz() {
 
 function renderQuestion(question) {
     questionElement.textContent = question.question;
-    button1Element.textContent = question.option1;
-    button2Element.textContent= question.option2;
-    button3Element.textContent = question.option3;
-    button4Element.textContent = question.answer;
+    button1Element.textContent = question.options[0];
+    button2Element.textContent= question.options[1];
+    button3Element.textContent = question.options[2];
+    button4Element.textContent = question.options[3];
 }
 
-startButton.addEventListener("click", startQuiz)
+function checkAnswer(button) {
+    console.log(button.textContent);
+    if(button.textContent === questionList[currentQuestion].answer) {
+        score++;
+        currentQuestion++;
+        if(currentQuestion >= questionList.length) {
+            renderEndPage();
+            return;
+        } else {
+            renderQuestion(questionList[currentQuestion]);
+        }
+        
+    } else {
+        //subtract timer
+        currentQuestion++;
+        if(currentQuestion >= questionList.length) {
+            renderEndPage();
+            return;
+        } else {
+            renderQuestion(questionList[currentQuestion]);
+        }
+    }
+}
+
+function renderEndPage() {
+    mainElement.classList.remove("hidden");
+    quizElement.classList.add("hidden");
+}
+
+startButton.addEventListener("click", startQuiz);
+button1Element.addEventListener("click", function() {checkAnswer(button1Element)});
+button2Element.addEventListener("click", function() {checkAnswer(button2Element)});
+button3Element.addEventListener("click", function() {checkAnswer(button3Element)});
+button4Element.addEventListener("click", function() {checkAnswer(button4Element)});
